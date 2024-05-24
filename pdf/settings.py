64df2445c3cfe5a4d6e2f0690abf4bc509f3e15c
Henry <hogbunike@gmail.com>
2024-05-24 21:34:32 +0100

    Prepared Project for Deployment in Railway

--- pdf/settings.py
+++ pdf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ud^f08!jqllftswq!##p!=xvq^mo57qxvfgdvg$$y*+**5n&l_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,6 +138,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOR = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
