1c0e6fe656480ad1a1c7d8f70f0afbacad462e81
Henry <hogbunike@gmail.com>
2024-05-26 14:51:00 +0100

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import PDFUploadView, PDFUploadFormView
+from .views import PDFUploadView
 
 urlpatterns = [
     path('upload/', PDFUploadView.as_view(), name='pdf-upload'),
-    path('upload-form/', PDFUploadFormView.as_view(), name='upload-form'),
 ]
--- pdf/urls.py
+++ pdf/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import PDFUploadFormView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', PDFUploadFormView.as_view(), name='upload-form'),
 ]
